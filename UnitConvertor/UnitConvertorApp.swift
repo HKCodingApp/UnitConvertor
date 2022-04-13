692323d0ba2d6b34b9de2fca180972fc716fbbac
Joël <joel.heckmann@reseau.eseo.fr>
2022-04-13 21:04:41 +0200

    Initial Commit

---
+++ UnitConvertor/UnitConvertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConvertorApp.swift
+//  UnitConvertor
+//
+//  Created by Joël on 13/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct UnitConvertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
